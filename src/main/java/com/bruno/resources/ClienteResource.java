9530e4ee279110722b0c22b44cd6ad4e078e06a4
Bruno Vidal <brunoav91@gmail.com>
2019-10-15 22:13:06 -0300

    Criando service cliente

--- src/main/java/com/bruno/resources/ClienteResource.java
+++ src/main/java/com/bruno/resources/ClienteResource.java
@@ -1,13 +1,25 @@
 package com.bruno.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.bruno.domain.Categoria;
 import com.bruno.domain.Cliente;
+import com.bruno.dto.ClienteDTO;
 import com.bruno.service.ClienteService;
 
 @RestController
@@ -22,6 +34,58 @@ public class ClienteResource {
 		Cliente cliente = clienteService.find(id);
 		
 		return ResponseEntity.ok().body(cliente);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO categoriaDTO){
+		
+		Cliente cliente=clienteService.fromDTO(categoriaDTO);
+		cliente=clienteService.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(cliente.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO categoriaDTO,@PathVariable Long id){
+		
+		Cliente cliente=clienteService.fromDTO(categoriaDTO);
+		cliente.setId(id);
+		cliente=clienteService.update(cliente);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		
+		clienteService.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> lista = clienteService.findAll();
+		List<ClienteDTO>listaDTO=lista.stream()
+				.map(cli->new ClienteDTO(cli)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listaDTO);
+	}
+	
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page",defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage",defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy",defaultValue="nome") String orderBy,
+			@RequestParam(value="direction",defaultValue="ASC") String direction) {
+		Page<Cliente> lista = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO>listaDTO=lista.map(cat -> new ClienteDTO(cat));
+				
+		
+		return ResponseEntity.ok().body(listaDTO);
 	}
 
 }
--- src/main/java/com/bruno/service/ClienteService.java
+++ src/main/java/com/bruno/service/ClienteService.java
@@ -1,10 +1,18 @@
 package com.bruno.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bruno.domain.Cliente;
+import com.bruno.dto.ClienteDTO;
 import com.bruno.repository.ClienteRepository;
+import com.bruno.service.exception.DataIntegrityException;
 import com.bruno.service.exception.ObjectNotFoundException;
 
 @Service
@@ -17,6 +25,45 @@ public class ClienteService {
 		Cliente cliente= clienteRepository.findById(id)
 				.orElseThrow(()-> new ObjectNotFoundException("Categoria não foi encontrada"));
 		return cliente;
+	}
+	
+	
+	public Cliente insert(Cliente categoria) {
+		return clienteRepository.save(categoria);
+	}
+	
+	public Cliente update(Cliente cliente) {
+
+		Cliente clienteBanco =find(cliente.getId());
+		atualizarCliente(clienteBanco,cliente);
+
+		return clienteRepository.save(clienteBanco);
+
+	}
+
+	public void delete(Long id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+
+		} catch (DataIntegrityViolationException ex) {
+			throw new DataIntegrityException("Não e possivel exclui porque a propiedades  associadas");
+		}
+	}
+	public List<Cliente>findAll(){
+		return clienteRepository.findAll(); 
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		return clienteRepository.findAll(PageRequest.of(page,linesPerPage,Direction.valueOf(direction), orderBy));
+	}
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(),clienteDTO.getNome() , clienteDTO.getEmail(),null, null);
+	}
+	
+	private void atualizarCliente(Cliente cliente,Cliente clienteDadosNovos) {
+		cliente.setNome(clienteDadosNovos.getNome());
+		cliente.setEmail(clienteDadosNovos.getEmail());
 	}
 	
 }

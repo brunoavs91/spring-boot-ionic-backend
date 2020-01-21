23e42af58d6eb802b77069e331afaa18e47a761c
Bruno Vidal <brunoav91@gmail.com>
2020-01-20 22:25:53 -0300

    Implementando segurança autenticacao

---
+++ src/main/java/com/bruno/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.bruno.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.bruno.domain.Cliente;
+import com.bruno.repository.ClienteRepository;
+import com.bruno.security.UserSS;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		Cliente cliente= clienteRepository.findByEmail(email);
+		if(cliente == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cliente.getId(),cliente.getEmail(),cliente.getSenha(),cliente.getPerfis());
+	}
+
+}

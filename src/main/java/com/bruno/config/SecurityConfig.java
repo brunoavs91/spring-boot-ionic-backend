257771ae0f26533c5f45dca1e721a654f3732370
Bruno Vidal <brunoav91@gmail.com>
2020-03-03 21:08:29 -0300

    Gerando token e validando

--- src/main/java/com/bruno/config/SecurityConfig.java
+++ src/main/java/com/bruno/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.bruno.security.JWTAuthenticationFilter;
+import com.bruno.security.JWTAuthorizationFilter;
 import com.bruno.security.JWTUtil;
 
 @Configuration
@@ -74,6 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
